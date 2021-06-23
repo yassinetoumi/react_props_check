eaa20aec181caec4214db434f2b41ba364b0049c
yassinetoumi <yassinetoumi11@hotmail.fr>
2021-06-23 04:43:12 +0100

    props

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import Component from  './profie/Component.js';
+import picture from './picture.gif';
+    const App = () => {
+      const alertMyInput = button => alert(button);
+      return (
+        <div>
+       
+                <Component fullname="Toumi Yassine is a React Developer also Adaptive ,Creatif and Hard Worker"
+                 alertMyInput={alertMyInput} 
+                
+                 version='16'/>
+   < img src={picture} alt="profil image" />
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+     
+      </div>
+     );
+  
+      }
 
 export default App;
---
+++ src/profie/Component.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import picture from '../picture.gif';
+import PropTypes from 'prop-types';
+
+const Component = props =>{
+
+    
+
+     return (
+        <div>
+             {props.fullname!=' ' ? (
+       <h1 style={{color: "red"}}> {props.fullname} </h1>
+     ) : (
+       <h1> we need that props</h1>
+     )}
+        <h2> {props.children}</h2>
+        <button onClick={() => props.alertMyInput(`Here is Toumi Yassine`)}>
+          ClickMe
+        </button>
+        <h3>React {props.version} </h3>
+          </div>
+        )
+     }
+        Component.propTypes = {
+      stringProp: PropTypes.string,
+     };
+    export default Component;
+   
